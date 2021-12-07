ae80bbe06217160e4a5f665ba47fef89da5293c6
ysw753 <81672833+ysw753@users.noreply.github.com>
2021-12-07 12:47:33 +0900

    Create 예산.py

---
+++ Programmers_Level1/src/Lv1/예산.py
@@ -0,0 +1,50 @@
+import sys
+
+d=[2,2,3,3]
+budget = 10
+d.sort()
+sum=0
+cnt=0
+for x in d:
+  sum+=x
+  if sum<=budget:
+    cnt+=1
+  else:
+    break
+print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 시간초과남
+# cnt=0
+# cnt=0
+# answer = 0
+# def DFS(L,sum):
+#         global cnt
+#         global answer
+#         if sum>budget:
+#           return
+#         if L==len(d):
+#             if budget>=sum:
+#                 if answer<cnt:
+#                     answer=cnt
+#         else:
+#             cnt+=1
+#             DFS(L+1,sum+d[L])
+#             cnt-=1
+#             DFS(L+1,sum)
+# DFS(0,0)
+# print(answer)
