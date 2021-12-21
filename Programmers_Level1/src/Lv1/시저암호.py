e8c57bbcd20abefcdf5ffd64f90e2f705162aa81
ysw753 <swyoo753@naver.com>
2021-12-21 22:12:41 +0900

    Create 시저암호.py

---
+++ Programmers_Level1/src/Lv1/시저암호.py
@@ -0,0 +1,12 @@
+import sys
+
+s = "AB"
+s = list(s)
+n = 1
+for i in range(len(s)):
+    if s[i].isupper:
+        s[i] = chr((ord(s[i])-ord('A')+n) % 26+ord('A'))
+    elif s[i].islower:
+        s[i] = chr((ord(s[i])-ord('a')+n) % 26+ord('a'))
+answer = ''.join(s)
+print(answer)
