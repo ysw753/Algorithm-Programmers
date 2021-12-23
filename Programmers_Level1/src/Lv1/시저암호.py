921273d2766755fb4d5c5f0d8d68fa6a4047667c
ysw753 <swyoo753@naver.com>
2021-12-23 20:12:41 +0900

    Update 시저암호.py

--- Programmers_Level1/src/Lv1/시저암호.py
+++ Programmers_Level1/src/Lv1/시저암호.py
@@ -4,9 +4,9 @@ s = "AB"
 s = list(s)
 n = 1
 for i in range(len(s)):
-    if s[i].isupper:
+    if s[i].isupper():
         s[i] = chr((ord(s[i])-ord('A')+n) % 26+ord('A'))
-    elif s[i].islower:
+    elif s[i].islower():
         s[i] = chr((ord(s[i])-ord('a')+n) % 26+ord('a'))
 answer = ''.join(s)
 print(answer)
